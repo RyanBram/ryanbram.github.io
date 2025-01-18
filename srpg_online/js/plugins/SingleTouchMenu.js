c6e7d7e39dcbb029784d9806058543daf3079b6b
RyanBram <ryanbram@gmail.com>
2025-01-18 11:59:49 +0700

    Bug fix

--- srpg_online/js/plugins/SingleTouchMenu.js
+++ srpg_online/js/plugins/SingleTouchMenu.js
@@ -1,22 +1,20 @@
 /*:
-* @target MZ
-* @plugindesc Enables single touch selection for menu items in RPG Maker MZ while preserving swipe scrolling and mouse functionality.
-* @author RyanBram
-* @help
-* This plugin allows menu items to be selected with a single touch and preserves swipe scrolling and mouse functionality.
-*/
+ * @target MZ
+ * @plugindesc Enables single touch selection for menu items in RPG Maker MZ while preserving swipe scrolling and mouse functionality.
+ * @author RyanBram
+ * @help
+ * This plugin allows menu items to be selected with a single touch and preserves swipe scrolling and mouse functionality.
+ */
 
 (() => {
-    Window_Selectable.prototype.isExcludedFromSingleTouch = function() {
-        return this instanceof Window_SkillList || 
-               this instanceof Window_ItemList || 
-               this instanceof Window_EquipItem;
+    Window_Selectable.prototype.isExcludedFromSingleTouch = function () {
+        return this instanceof Window_SkillList || this instanceof Window_ItemList || this instanceof Window_EquipItem;
     };
     const _Window_Selectable_processTouch = Window_Selectable.prototype.processTouch;
-    Window_Selectable.prototype.processTouch = function() {
+    Window_Selectable.prototype.processTouch = function () {
         if (this.isExcludedFromSingleTouch()) {
-            _Window_Selectable_processTouch.call(this)
-        } else{
+            _Window_Selectable_processTouch.call(this);
+        } else {
             if (this.isOpenAndActive()) {
                 if (TouchInput.isTriggered() && this.isTouchedInsideFrame()) {
                     // Check if the input is from touch screen and not from the mouse
@@ -46,32 +44,31 @@
                 }
             }
         }
-
     };
 
-    Window_Selectable.prototype.onSingleTouch = function() {
+    Window_Selectable.prototype.onSingleTouch = function () {
         const localX = this.canvasToLocalX(TouchInput.x);
         const localY = this.canvasToLocalY(TouchInput.y);
         const hitIndex = this.hitTest(localX, localY);
         if (hitIndex >= 0) {
             this.select(hitIndex);
-            setTimeout(() => {
-                this.processOk();
-            }, 125); // 125 milliseconds delay
+            //setTimeout(() => {
+            this.processOk();
+            //}, 125); // 125 milliseconds delay
         }
     };
 
-    Window_Selectable.prototype.canvasToLocalX = function(x) {
+    Window_Selectable.prototype.canvasToLocalX = function (x) {
         const shape = this.getBounds();
         return x - shape.x;
     };
 
-    Window_Selectable.prototype.canvasToLocalY = function(y) {
+    Window_Selectable.prototype.canvasToLocalY = function (y) {
         const shape = this.getBounds();
         return y - shape.y;
     };
 
-    Window_Selectable.prototype.hitTest = function(x, y) {
+    Window_Selectable.prototype.hitTest = function (x, y) {
         for (let i = 0; i < this.maxItems(); i++) {
             const rect = this.itemRectWithPadding(i);
             if (x >= rect.x && y >= rect.y && x < rect.x + rect.width && y < rect.y + rect.height) {
